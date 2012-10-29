c1c53c5714edd2848d01d9d618324c0c99a69014
Daniel Stepp <willstepp@gmail.com>
2012-10-29 19:42:52 -0400

    initial upload

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+
+gem 'rails', '3.2.8'
+gem 'mongoid', '~> 3.0.0'
+
+group :deveopment do
+  gem 'thin'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasybath::Application.config.secret_token = '8dff4702d5c0cf6c447364594f7e788496a51bfdb8dca9a5c9cdbd46893c593172b544d97de8aeaedce17d94b3a80c58f0bc05fad15d6555ea44e09b9ec1d2d9'
